00a8d0f515f6d7aabd31f14ffe2ebd491534705d
bennypc <52959626+bennypc@users.noreply.github.com>
2022-04-18 14:01:07 -0700

    Created new sidenav bar

---
+++ main.js
@@ -0,0 +1,9 @@
+''/* Open the sidenav */
+function openNav() {
+    document.getElementById("sidenav").style.width = "100%";
+}
+  
+/* Close/hide the sidenav */
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+}
